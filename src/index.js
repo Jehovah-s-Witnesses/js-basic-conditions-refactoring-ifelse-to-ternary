be1803ec33e88c26b8e57c74da8b94289abee74a
gribVolnushka <burgerqueen98@gmail.com>
2024-09-24 12:32:40 +0200

    changed structure if-else to ternary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const phoneNumber = prompt('Enter your phone number');
 let phoneCode;
 
-if (phoneNumber.includes('+380')) {
+/*if (phoneNumber.includes('+380')) {
   phoneCode = 'Ukraine';
 } else {
   phoneCode = 'Other';
 }
+
+alert(phoneCode);
+*/
+
+phoneNumber.includes('+380') ? phoneCode = 'Ukraine' : phoneCode = 'Other';
 
 alert(phoneCode);
